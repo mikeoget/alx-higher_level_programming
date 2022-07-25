021755748f74cdcf13b91066ffe2d643c34860c6
Michael Ogeto <84995011+mikeoget@users.noreply.github.com>
2022-07-25 13:22:51 +0300

    commit zero

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,11 @@
-tomorrow morning
+
+#!/usr/bin/python3
+"""
+a reading function
+"""
+
+
+def read_file(filename=""):
+    """reads files"""
+    with open(filename, mode="r", encoding="utf-8") as mefile:
+        print(mefile.read(), end="")
